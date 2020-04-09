659ad3e7c9fd136186dd1a16c654814a2d634d5a
Andrew Lister <andrew.lister@stfc.ac.uk>
2020-04-09 18:36:10 +0100

    Add robin bounds

--- TTiP/problem_mixins/boundaries_mixin.py
+++ TTiP/problem_mixins/boundaries_mixin.py
@@ -66,7 +66,6 @@ class BoundaryMixin:
     def add_boundary(self, boundary_type, **kwargs):
         """
         Generic method for adding boundaries to the problem.
-        Currently only "dirichlet" boundaries are accepted.
 
         To view documentation for boundary types, see the appropriate function.
 
@@ -74,14 +73,15 @@ class BoundaryMixin:
             boundary_type (string):
                 The name of the boundary condition:
                     - "dirichlet"
-                    - "robin" (in progress)
+                    - "robin"
                     - "neuman" (in progress)
                 These can be given in full, or as shortened names (e.g. "dir")
 
         Raises:
             ValueError: When the boundary is not in the supported names list.
         """
-        dispatcher = {'dirichlet': self.add_dirichlet}
+        dispatcher = {'dirichlet': self.add_dirichlet,
+                      'robin': self.add_robin}
         for name, func in dispatcher.items():
             if name.startswith(boundary_type):
                 func(**kwargs)
@@ -123,6 +123,47 @@ class BoundaryMixin:
                 self.a += integrand*ds(surface)
 
         self.bcs.append(dbc)
+        self._has_boundary = True
+
+    def add_robin(self, alpha, g, surface):
+        """
+        Adds robin boundary conditions to the problem.
+
+        The Robin condition is a mixed codition and is defined by:
+            dot(grad(T), n) = g - alpha*T
+
+        Args:
+            g (Function, int, or float):
+                The function to apply on the boundary as per the above formula.
+            alpha (Function, int, or float):
+                The function to apply on the boundary as per the above formula.
+            surface (int or list of int):
+                The index of the boundary to apply the condition to.
+        """
+        # Explicitly check against False as None should not be caught here.
+        if self._has_boundary is False:
+            raise AttributeError('Cannot add boundary after declaring that '
+                                 'there are no boundaries')
+
+        if isinstance(g, (float, int)):
+            g = Constant(g)
+        if isinstance(alpha, (float, int)):
+            alpha = Constant(g)
+
+        a_integrand = alpha*self.v*self.T
+        L_integrand = self.v*g
+
+        if surface == 'all':
+            self.a += a_integrand*ds
+            self.L += L_integrand*ds
+        else:
+            try:
+                self.a += sum(a_integrand*ds(s) for s in surface)
+                self.L += sum(L_integrand*ds(s) for s in surface)
+            except TypeError:
+                self.a += a_integrand*ds(surface)
+                self.L += L_integrand*ds(surface)
+
         self._has_boundary = True
 
     def set_no_boundary(self):
