1d2d2fd7828ead440cd3d2351970f07e4943e4e3
Andrew Lister <andrew.lister@stfc.ac.uk>
2020-05-20 15:27:50 +0100

    Fix autodoc - take 2

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 kwargs = {'name': 'TTiP',
           'version': '0.0',
@@ -12,9 +12,12 @@ kwargs = {'name': 'TTiP',
 entry_points = {'console_scripts': ['ttip = TTiP.cli.main:main']}
 packages = find_packages(exclude=('*mock*', '*test*'))
 package_data = {'TTiP': ['resources/default_config.ini']}
+install_requires = ['scipy',
+                    'numpy']
 
 kwargs['entry_points'] = entry_points
 kwargs['packages'] = packages
 kwargs['package_data'] = package_data
+kwargs['install_requires'] = install_requires
 
 setup(**kwargs)
